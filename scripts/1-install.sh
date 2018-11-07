f64239aa39e13b92ec8249b13dfa2ca5bc2cc04b
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-11-07 06:50:55 -0300

    variable

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-PROMETHEUS_VERSION="2.2.1"
+# debug
+set -xv
+# variable
+PROMETHEUS_VERSION="2.5.0"
 wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 tar -xzvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 cd prometheus-${PROMETHEUS_VERSION}.linux-amd64/
